da9392b40e94daf71e1a6054ad8c34c276f7d44d
wolfgang <einstenwolfgang@qq.com>
2018-03-26 11:54:51 +0800

    web骨架

---
+++ www/app.py
@@ -0,0 +1,19 @@
+import asyncio,os,json,time
+from datetime import datetime
+from aiohttp import web
+import logging; logging.basicConfig(level=logging.INFO)
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9001)
+    logging.info('server started at port 9001')
+    return srv
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', headers={'content-type':'text/html'})
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
